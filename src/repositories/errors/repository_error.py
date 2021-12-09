b2acd99236ea9e8efb72d0281cd158477a2e8df1
VitorMartin <vitor.martin.simoni@gmail.com>
2021-12-09 19:08:54 -0300

    Passing msg in generic RepositoryError

--- src/repositories/errors/repository_error.py
+++ src/repositories/errors/repository_error.py
@@ -1,6 +1,9 @@
 class RepositoryError(Exception):
-    def __init__(self):
-        super().__init__('Repository error.')
+    def __init__(self, msg=None):
+        if msg:
+            super().__init__(msg)
+        else:
+            super().__init__('Repository error.')
 
 
 class NotFoundError(Exception):
