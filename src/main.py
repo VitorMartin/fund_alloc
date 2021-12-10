c2b20274dd6716591a495e78c2aa18fc6f76e377
VitorMartin <vitor.martin.simoni@gmail.com>
2021-12-10 16:51:24 -0300

    Change Init sequence to default values

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from src.init import Init
-from src.models.enums.config import *
 
 if __name__ == '__main__':
-    Init(_REPO_TYPE=REPO_TYPE.MOCK, _CTRL_TYPE=CTRL_TYPE.FASTAPI)()
+    Init()()
 
     pass
