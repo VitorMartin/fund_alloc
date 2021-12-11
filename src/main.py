e2fb066732674e3d1758f336d6d3ffe8f3e3f69c
VitorMartin <vitor.martin.simoni@gmail.com>
2021-12-11 17:14:09 -0300

    Add requirements.txt

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
+import sys
+
 from src.init import Init
 
 if __name__ == '__main__':
+    print(sys.version_info)
+    if sys.version_info[0:2] != (3, 9):
+        raise Exception('Requires python 3.9')
+
     Init()()
 
     pass
